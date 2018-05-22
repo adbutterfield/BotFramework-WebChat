497719ac8cf29315743d39519114b83ee4588ef2
Adam Butterfield <adb707@yahoo.com>
2018-05-22 09:28:53 +0900

    Format to 2 spaces

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import * as konsole from './Konsole';
 export type AppProps = ChatProps;
 
 export const App = (props: AppProps, container: HTMLElement) => {
-    konsole.log("BotChat.App props", props);
-    ReactDOM.render(React.createElement(AppContainer, props), container);
+  konsole.log("BotChat.App props", props);
+  ReactDOM.render(React.createElement(AppContainer, props), container);
 }
 
 const AppContainer = (props: AppProps) =>
-    <div className="wc-app">
-        <Chat { ...props } />
-    </div>;
+  <div className="wc-app">
+    <Chat {...props} />
+  </div>;
--- src/getTabIndex.ts
+++ src/getTabIndex.ts
@@ -4,21 +4,21 @@ const IS_FIREFOX = /Firefox\//i.test(navigator.userAgent);
 const IS_IE = /Trident\//i.test(navigator.userAgent);
 
 export function getTabIndex(element: HTMLElement) {
-    const { tabIndex } = element;
+  const { tabIndex } = element;
 
-    if (IS_IE) {
-        const tabIndexAttribute = element.attributes.getNamedItem('tabindex');
+  if (IS_IE) {
+    const tabIndexAttribute = element.attributes.getNamedItem('tabindex');
 
-        if (!tabIndexAttribute || !tabIndexAttribute.specified) {
-            return ~IE_FOCUSABLE_LIST.indexOf(element.nodeName.toLowerCase()) ? 0 : null;
-        }
-    } else if (!~tabIndex) {
-        const attr = element.getAttribute('tabindex');
-
-        if (attr === null || (attr === '' && !IS_FIREFOX)) {
-            return null;
-        }
+    if (!tabIndexAttribute || !tabIndexAttribute.specified) {
+      return ~IE_FOCUSABLE_LIST.indexOf(element.nodeName.toLowerCase()) ? 0 : null;
     }
+  } else if (!~tabIndex) {
+    const attr = element.getAttribute('tabindex');
 
-    return tabIndex;
+    if (attr === null || (attr === '' && !IS_FIREFOX)) {
+      return null;
+    }
+  }
+
+  return tabIndex;
 };
